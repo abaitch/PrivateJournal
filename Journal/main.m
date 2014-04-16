a2aa5363e64628b7605c7f626d71185cd6b06071
Adam Baitch <ajb32@optonline.net>
2014-04-16 12:56:51 -0400

    Initial Commit

---
+++ Journal/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Journal
+//
+//  Created by Adam Baitch on 4/16/14.
+//  Copyright (c) 2014 Adam Baitch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AJBAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AJBAppDelegate class]));
+    }
+}
---
+++ JournalTests/JournalTests.m
@@ -0,0 +1,34 @@
+//
+//  JournalTests.m
+//  JournalTests
+//
+//  Created by Adam Baitch on 4/16/14.
+//  Copyright (c) 2014 Adam Baitch. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JournalTests : XCTestCase
+
+@end
+
+@implementation JournalTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
