5dd61e67eb03f7514f85b5366de7dc0879e84c60
Hunty Dumpty <hunter.horsley@gmail.com>
2014-05-09 14:32:12 -0400

    adding old views and tables

---
+++ Journal/AJBMyLogsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AJBMyLogsTableViewController.h
+//  Journal
+//
+//  Created by Hunter Horsley on 5/8/14.
+//  Copyright (c) 2014 Adam Baitch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJBMyLogsTableViewController : UITableViewController
+
+@end
---
+++ Journal/AJBOLDLogPhotoViewController.h
@@ -0,0 +1,21 @@
+//
+//  AJBOLDLogPhotoViewController.h
+//  Journal
+//
+//  Created by Hunter Horsley on 5/9/14.
+//  Copyright (c) 2014 Adam Baitch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AJBOLDLogPhotoViewController : UIViewController
+
+@property (nonatomic, retain) NSString * entryTitle;
+@property (nonatomic, retain) NSString * comments;
+@property (nonatomic) float latitude;
+@property (nonatomic) float longitude;
+@property (nonatomic, retain) NSString * filePath;
+@property (nonatomic, retain) NSString * fileType;
+@property (nonatomic, retain) NSDate * date;
+
+@end
